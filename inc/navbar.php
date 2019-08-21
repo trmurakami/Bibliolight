3266d43ce9a54e0a927096cae8b16ec3b1690f5d
Tiago <tiago@bdpife2.sibi.usp.br>
2019-08-21 07:45:56 -0300

    Initial

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,8 +1,15 @@
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
     <h5 class="my-0 mr-md-auto font-weight-normal"><a href="index.php">Biblioteca Bibliolight</a></h5>
     <nav class="my-2 my-md-0 mr-md-3">
+    <form class="form-inline my-2 my-lg-0" action="search.php" method="get">
+      <input class="form-control mr-sm-2" type="search" placeholder="Termo de busca" aria-label="Pesquisar" name="search[]">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+    </form>    
+    </nav>
+    <nav class="my-2 my-md-0 mr-md-3">
         <a class="btn btn-outline-info" href="tools/export.php?format=table">Exportar registros em CSV</a>
         <a class="btn btn-outline-success" href="editor.php">Criar registro</a>
     </nav>
+    
     <a class="btn btn-outline-primary" href="login.php">Login</a>
 </div>

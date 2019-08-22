cb44de1db1b2a9c9b6c073020500759a16ec89b2
Tiago <tiago@bdpife2.sibi.usp.br>
2019-08-22 09:14:11 -0300

    Initial

--- editor.php
+++ editor.php
@@ -240,7 +240,7 @@ if (!isset($classificationsValue)) {
     <div class="custom-file">
         <input type="file" class="custom-file-input" id="customFile" name="cover">
         <label class="custom-file-label" for="customFile">Selecionar arquivo de capa. Somente com nome usando o mesmo ISBN e jpg. Ex: 9788585637323.jpg)</label>
-    </div>
+    </div> 
     <br/><br/>              
     <button type="submit" class="btn btn-primary">Salvar</button>
     </form>
@@ -256,8 +256,8 @@ if (!isset($classificationsValue)) {
 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>           
-
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> 
+  <script src="https://getbootstrap.com/docs/4.3/assets/js/docs.min.js"></script>
 
   </body>
 </html>

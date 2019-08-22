17941bcdd298b35a5e342ef5472630f6d70f987e
Tiago <tiago@bdpife2.sibi.usp.br>
2019-08-22 10:35:17 -0300

    Initial

--- search.php
+++ search.php
@@ -131,9 +131,10 @@ if (isset($_GET["sort"])) {
                             $_GET["search"] = null;
                         }
                             $facets->facet("contributor", 50, "Autores", null, "_term", $_GET["search"], true);
-                            $facets->facet("publisher", 50, "Editora", null, "_term", $_GET["search"], true);
+                            $facets->facet("publisher", 50, "Editoras", null, "_term", $_GET["search"], true);
                             $facets->facet("date", 50, "Data de publicação", "desc", "_term", $_GET["search"], true);
                             $facets->facet("subjects", 50, "Assuntos", null, "_term", $_GET["search"], true);
+                            $facets->facet("languages", 50, "Idiomas", null, "_term", $_GET["search"], true);
                         ?>
                         
                     </div>

99f33d5f483a868b5ca6b34e4d9347ce18e911d6
Tiago Murakami <trmurakami@gmail.com>
2020-08-13 18:39:46 +0000

    Correção tirado canonical

--- index.php
+++ index.php
@@ -11,8 +11,6 @@
         <meta name="author" content="Tiago Murakami">
         <title><?php echo $library_name; ?></title>
 
-        <link rel="canonical" href="https://github.com/trmurakami/bibliolight">
-
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
